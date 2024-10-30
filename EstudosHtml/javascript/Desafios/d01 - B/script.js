e53a2d2e333be006e14bfd37a0cc50ae1f70803c
joao_maffezzolli <joao.l09@aluno.ifsc.edu.br>
2024-10-30 16:04:19 -0300

    Verificador de Idade

--- EstudosHtml/javascript/Desafios/d01 - B/script.js
+++ EstudosHtml/javascript/Desafios/d01 - B/script.js
@@ -3,7 +3,8 @@ function verificar(){
     var ano = data.getFullYear()
     var fAno = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    var img = window.document.getElementById('imagem')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if(fAno.value.length == 0 || fAno.value > ano){
         window.alert('[ERRO] Verifique os dados')
     }else{
@@ -13,10 +14,29 @@ function verificar(){
         
         if(fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src', 'imagens/homembebe.jpg')
+            } else if (idade < 21){
+                img.setAttribute('src', 'imagens/homemjovem.jpg')
+            } else if (idade < 50){
+                img.setAttribute('src', 'imagens/homemadulto.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/homemvelho.jpg')
+            }
         }else if(fsex[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src', 'imagens/mulherbebe.jpg')
+            } else if (idade < 21){
+                img.setAttribute('src', 'imagens/mulherjovem.jpg')
+            } else if (idade < 50){
+                img.setAttribute('src', 'imagens/mulheradulta.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/mulhervelha.jpg')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
