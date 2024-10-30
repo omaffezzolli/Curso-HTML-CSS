fbfd7d4a907c1ed6978a4d5fdeae3a62f96b2835
joao_maffezzolli <joao.l09@aluno.ifsc.edu.br>
2024-10-30 15:43:21 -0300

    Desafio 2

--- EstudosHtml/javascript/Desafios/d01 - B/script.js
+++ EstudosHtml/javascript/Desafios/d01 - B/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    var img = window.document.getElementById('imagem')
+    if(fAno.value.length == 0 || fAno.value > ano){
+        window.alert('[ERRO] Verifique os dados')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        
+        if(fsex[0].checked){
+            genero = 'Homem'
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
